5d7684279bda5f2463ecdf1a8ddf3b34837f02bb
g2413683931 <112173138+g2413683931@users.noreply.github.com>
2022-08-27 16:43:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -184,11 +184,11 @@ def get_waxy():
     }
     r = get(url, headers=headers)
     b = r.json()["newslist"]
-    c=b[0]
+    c=b[8]
     qinghua=c["content"]
     return waxy
 
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en,qinghua,riji,kqzl,xzys,jrgk):
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_en,qinghua,riji,kqzl,xzys,jrgk,waxy):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
